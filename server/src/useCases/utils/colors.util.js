b5d4915340869b89cfa213314289a5a5c28f0cba
HenriqueArtur <henriqueacg@gmail.com>
2022-04-10 17:42:01 -0300

    :sparkles: - add color functions

---
+++ server/src/useCases/utils/colors.util.js
@@ -0,0 +1,137 @@
+/* ----------------------> FOREGROUND <---------------------- */
+export function default_color(text) {
+  return `\x1b[39m${text}\x1b[0m`
+}
+
+export function black_color(text) {
+  return `\x1b[30m${text}\x1b[0m`
+}
+
+export function red_color(text) {
+  return `\x1b[31m${text}\x1b[0m`
+}
+
+export function green_color(text) {
+  return `\x1b[32m${text}\x1b[0m`
+}
+
+export function yellow_color(text) {
+  return `\x1b[33m${text}\x1b[0m`
+}
+
+export function blue_color(text) {
+  return `\x1b[34m${text}\x1b[0m`
+}
+
+export function magenta_color(text) {
+  return `\x1b[35m${text}\x1b[0m`
+}
+
+export function cyan_color(text) {
+  return `\x1b[36m${text}\x1b[0m`
+}
+
+export function light_gray_color(text) {
+  return `\x1b[37m${text}\x1b[0m`
+}
+
+export function dark_gray_color(text) {
+  return `\x1b[90m${text}\x1b[0m`
+}
+
+export function light_red_color(text) {
+  return `\x1b[91m${text}\x1b[0m`
+}
+
+export function light_green_color(text) {
+  return `\x1b[92m${text}\x1b[0m`
+}
+
+export function light_yellow_color(text) {
+  return `\x1b[93m${text}\x1b[0m`
+}
+
+export function light_blue_color(text) {
+  return `\x1b[94m${text}\x1b[0m`
+}
+
+export function light_magenta_color(text) {
+  return `\x1b[95m${text}\x1b[0m`
+}
+
+export function light_cyan_color(text) {
+  return `\x1b[96m${text}\x1b[0m`
+}
+
+export function white_color(text) {
+  return `\x1b[97m${text}\x1b[0m`
+}
+
+/* ----------------------> BACKGROUND <---------------------- */
+export function bg_default_color(text) {
+  return `\x1b[49m${text}\x1b[0m`
+}
+
+export function bg_black_color(text) {
+  return `\x1b[40m${text}\x1b[0m`
+}
+
+export function bg_red_color(text) {
+  return `\x1b[41m${text}\x1b[0m`
+}
+
+export function bg_green_color(text) {
+  return `\x1b[42m${text}\x1b[0m`
+}
+
+export function bg_yellow_color(text) {
+  return `\x1b[43m${text}\x1b[0m`
+}
+
+export function bg_blue_color(text) {
+  return `\x1b[44m${text}\x1b[0m`
+}
+
+export function bg_magenta_color(text) {
+  return `\x1b[45m${text}\x1b[0m`
+}
+
+export function bg_cyan_color(text) {
+  return `\x1b[46m${text}\x1b[0m`
+}
+
+export function bg_light_gray_color(text) {
+  return `\x1b[47m${text}\x1b[0m`
+}
+
+export function bg_dark_gray_color(text) {
+  return `\x1b[100m${text}\x1b[0m`
+}
+
+export function bg_light_red_color(text) {
+  return `\x1b[101m${text}\x1b[0m`
+}
+
+export function bg_light_green_color(text) {
+  return `\x1b[102m${text}\x1b[0m`
+}
+
+export function bg_light_yellow_color(text) {
+  return `\x1b[103m${text}\x1b[0m`
+}
+
+export function bg_light_blue_color(text) {
+  return `\x1b[104m${text}\x1b[0m`
+}
+
+export function bg_light_magenta_color(text) {
+  return `\x1b[105m${text}\x1b[0m`
+}
+
+export function bg_light_cyan_color(text) {
+  return `\x1b[106m${text}\x1b[0m`
+}
+
+export function bg_white_color(text) {
+  return `\x1b[107m${text}\x1b[0m`
+}
