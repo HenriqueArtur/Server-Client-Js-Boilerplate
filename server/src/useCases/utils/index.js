9f7ae1968be7faf1944534f60544f2eefd23f6c1
HenriqueArtur <henriqueacg@gmail.com>
2022-04-10 15:59:45 -0300

    :sparkles: - exports validations

---
+++ server/src/useCases/utils/index.js
@@ -0,0 +1,13 @@
+import {
+  validate_email,
+  validate_cpf,
+  validate_cnpj,
+  validate_phone,
+} from "./validate_forms.util";
+
+export {
+  validate_email,
+  validate_cpf,
+  validate_cnpj,
+  validate_phone,
+}
